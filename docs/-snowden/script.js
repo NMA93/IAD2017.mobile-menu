cfece53f4174a3d1cbc8384dad0a0eaba4eea757
NMA93 <mail@nilsmaeder.ch>
2018-12-26 18:25:12 +0100

    mobile_menu

--- docs/-snowden/script.js
+++ docs/-snowden/script.js
@@ -1,3 +1,6 @@
+let open = document.getElementById("open")
+let close = document.getElementById("close")
+
 var nav_mobile = document.getElementById("myheader");
 var sticky = nav_mobile.offsetTop;
 
@@ -12,3 +15,18 @@ function myFunction() {
 window.addEventListener('scroll', function() {
   myFunction()
 });
+
+
+function menu_open() {
+  var open = document.getElementById("open");
+  overlay.classList.remove("hidden");
+}
+
+function menu_close() {
+  var close = document.getElementById("close");
+  overlay.classList.add("hidden");
+}
+
+
+open.addEventListener("click", menu_open);
+close.addEventListener("click", menu_close);
