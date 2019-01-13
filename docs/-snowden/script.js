f424ac1bf5f1b41fd60caadfd279a6e206e95d26
NMA93 <mail@nilsmaeder.ch>
2019-01-13 15:40:19 +0100

    menu

--- docs/-snowden/script.js
+++ docs/-snowden/script.js
@@ -3,33 +3,6 @@ let close = document.getElementById("close")
 
 var nav_mobile = document.getElementById("myheader");
 var nav_mobile_overlay = document.getElementById("myheader_overlay");
-var sticky = nav_mobile.offsetTop;
-var sticky_overlay = nav_mobile_overlay.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    nav_mobile.classList.add("sticky");
-  } else {
-    nav_mobile.classList.remove("sticky");
-  }
-}
-
-window.addEventListener('scroll', function() {
-  myFunction()
-});
-
-function myFunction_overlay() {
-  if (window.pageYOffset > sticky_overlay) {
-    nav_mobile_overlay.classList.add("sticky");
-  } else {
-    nav_mobile_overlay.classList.remove("sticky");
-  }
-}
-
-window.addEventListener('scroll', function() {
-  myFunction_overlay()
-});
-
 
 function menu_open() {
   var open = document.getElementById("open");
